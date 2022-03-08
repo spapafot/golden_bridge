066eaa55b08937847136d2b744d2c0468a08a8d3
Stratos Papafotiou <spapafot@gmail.com>
2022-03-08 10:27:24 +0200

    Update trucks.py

--- trucks.py
+++ trucks.py
@@ -2,35 +2,49 @@ import pandas as pd
 import pdfplumber
 import re
 
-fname = "20210062.pdf"
-final = []
-text = ""
+def main(fname):
+    
+    final = []
+    text = ""
+    
+    with pdfplumber.open(fname) as pdf:
+        for page in pdf.pages:
+            text += page.extract_text()
 
-with pdfplumber.open(fname) as pdf:
-    for page in pdf.pages:
-        text += page.extract_text()
+    with open("sample.txt", "w", encoding='utf-8') as text_file:
+        text_file.write(text)
 
-with open("sample.txt", "w", encoding='utf-8') as text_file:
-    text_file.write(text)
+    with open("sample.txt", "r", encoding='utf-8') as txt:
+        text_file_ = txt.readlines()
 
-with open("sample.txt", "r", encoding='utf-8') as txt:
-    text_file_ = txt.readlines()
+    # Only unique line identifier is "|F"
+    
+    for i in text_file_:
+        if "¦F" in i:
+            if "VESSEL" not in i:
+                i = i[1:]
+                a = i.split("DEBIT", maxsplit=1)
+                b = re.split("GOLDEN |¦ ", a[0])
+                final.append(b)
 
-for i in text_file_:
-    if "¦F" in i:
-        if "VESSEL" not in i:
-            i = i[1:]
-            a = i.split("DEBIT", maxsplit=1)
-            b = re.split("GOLDEN |¦ ", a[0])
-            final.append(b)
+    data = pd.DataFrame(final)
+    
+    # Duplicates are canceled tickets (eg F54421 +200 and F54421 -200) so discard them
+    
+    data = data.drop_duplicates(subset=[0], keep=False)
+    data[1] = data[1].str[1:]
 
-data = pd.DataFrame(final)
-data = data.drop_duplicates(subset=[0], keep=False)
-data[1] = data[1].str[1:]
+    for i in range(3, 8):
+        data[i] = data[i].str.replace(",", ".").astype(float)
 
-for i in range(3, 8):
-    data[i] = data[i].str.replace(",", ".").astype(float)
+    # Group by company can be done with cashier, more accurate as it is agency handled
+    
+    with pd.ExcelWriter("trucks.xlsx", "xlsxwriter") as writer:
+        pd.DataFrame(data).to_excel(writer, index=None)
 
-with pd.ExcelWriter("trucks.xlsx", "xlsxwriter") as writer:
-    pd.DataFrame(data).to_excel(writer, index=None)
-
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Filename')
+    parser.add_argument('-f', type=str, required=True, help="Enter the filename in '1607BRIG.pdf' format")
+    args = parser.parse_args()
+    main(args.fname)
